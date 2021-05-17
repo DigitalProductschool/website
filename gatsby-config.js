e49386a032b06a26b6fdbedd71e6721359dfb852
bsinoimeri <b.sinoimeri@hotmail.com>
2021-05-17 18:58:17 +0200

    Gatsby plugin no JS

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,8 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
+    'gatsby-plugin-no-javascript',
+    'gatsby-plugin-no-javascript-utils',
   ],
 };
 
